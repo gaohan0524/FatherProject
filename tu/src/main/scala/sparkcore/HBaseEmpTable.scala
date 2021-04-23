26144a9ea226824585d366d3e45beeae6742bebb
gaohan0524 <1073554580@qq.com>
2021-04-23 17:26:13 +0800

    210423

---
+++ tu/src/main/scala/sparkcore/HBaseEmpTable.scala
@@ -0,0 +1,49 @@
+package sparkcore
+
+import org.apache.hadoop.hbase.HBaseConfiguration
+import org.apache.hadoop.hbase.client.Put
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable
+import org.apache.hadoop.hbase.mapred.TableOutputFormat
+import org.apache.hadoop.hbase.util.Bytes
+import org.apache.hadoop.mapred.JobConf
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+ *  20210418-20210425 HBase和Hive关联操作【以emp表为例】
+ *  案例二:
+ *  在HBase表中的数据，可以使用Hive SQL完成统计分析(deptno分组求每个组的人数)
+ */
+object HBaseEmpTable {
+	def main(args: Array[String]): Unit = {
+
+		val SparkConf = new SparkConf().setMaster("local[4]").setAppName("HBaseRDD")
+		val sc = new SparkContext(SparkConf)
+
+		val conf = HBaseConfiguration.create()
+
+		// 插入数据
+		// emp.empno emp.ename	emp.job	emp.mgr	emp.hiredate emp.sal emp.comm emp.deptno
+		val dataRDD = sc.textFile("tu/input/emp.txt").map(x => x.split(" "))
+		val putRDD = dataRDD.map{ x => {
+			val put = new Put(Bytes.toBytes(x(0)))
+			put.addColumn(Bytes.toBytes("info"), Bytes.toBytes("ename"), Bytes.toBytes(x(1)))
+			put.addColumn(Bytes.toBytes("info"), Bytes.toBytes("job"), Bytes.toBytes(x(2)))
+			put.addColumn(Bytes.toBytes("info"), Bytes.toBytes("mgr"), Bytes.toBytes(x(3)))
+			put.addColumn(Bytes.toBytes("info"), Bytes.toBytes("hiredate"), Bytes.toBytes(x(4)))
+			put.addColumn(Bytes.toBytes("info"), Bytes.toBytes("sal"), Bytes.toBytes(x(5)))
+			put.addColumn(Bytes.toBytes("info"), Bytes.toBytes("comm"), Bytes.toBytes(x(6)))
+			put.addColumn(Bytes.toBytes("info"), Bytes.toBytes("deptno"), Bytes.toBytes(x(7)))
+
+			(new ImmutableBytesWritable(Bytes.toBytes(x(0))), put)
+			}
+		}
+
+		val jobConf = new JobConf(conf)
+		jobConf.setOutputFormat(classOf[TableOutputFormat])
+		jobConf.set(TableOutputFormat.OUTPUT_TABLE, "emp")
+
+		putRDD.saveAsHadoopDataset(jobConf)
+
+		sc.stop()
+	}
+}
--- tu/src/main/scala/sparkcore/HBaseRDD.scala
+++ tu/src/main/scala/sparkcore/HBaseRDD.scala
@@ -1,10 +1,12 @@
 package sparkcore
 
 import org.apache.hadoop.hbase.{CellUtil, HBaseConfiguration}
-import org.apache.hadoop.hbase.client.Result
+import org.apache.hadoop.hbase.client.{Put, Result}
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable
+import org.apache.hadoop.hbase.mapred.TableOutputFormat
 import org.apache.hadoop.hbase.mapreduce.TableInputFormat
 import org.apache.hadoop.hbase.util.Bytes
+import org.apache.hadoop.mapred.JobConf
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
@@ -23,22 +25,35 @@ object HBaseRDD {
 		val conf = HBaseConfiguration.create()
 		conf.set(TableInputFormat.INPUT_TABLE, "stu")
 
+		// 1.插入数据
+		val dataRDD = sc.makeRDD(List(("1004", "wangwu", "15"), ("1005", "zhaoliu", "20")))
+		val putRDD = dataRDD.map{
+			case (rowKey, name, age) =>
+				val put = new Put(Bytes.toBytes(rowKey))
+				put.addColumn(Bytes.toBytes("info"), Bytes.toBytes("name"), Bytes.toBytes(name))
+				put.addColumn(Bytes.toBytes("info"), Bytes.toBytes("age"), Bytes.toBytes(age))
+				(new ImmutableBytesWritable(Bytes.toBytes(rowKey)), put)
+		}
+
+		val jobConf = new JobConf(conf)
+		jobConf.setOutputFormat(classOf[TableOutputFormat])
+		jobConf.set(TableOutputFormat.OUTPUT_TABLE, "stu")
+
+		putRDD.saveAsHadoopDataset(jobConf)
+
+		// 2.读取数据
 		val HBaseRDD = sc.newAPIHadoopRDD(conf, classOf[TableInputFormat],
 			classOf[ImmutableBytesWritable], classOf[Result])
 
 		HBaseRDD.foreach({
-			case (rowKey, result) => {
+			case (rowKey, result) =>
 				result.rawCells().foreach(cell => {
-					println(Bytes.toString(rowKey.get()) + "=" +
-					Bytes.toString(CellUtil.cloneValue(cell)))
+					println("Row=" + Bytes.toString(rowKey.get()) + " column=" +
+							Bytes.toString(CellUtil.cloneFamily(cell)) + ":" +
+							Bytes.toString(CellUtil.cloneQualifier(cell)) + " value=" +
+							Bytes.toString(CellUtil.cloneValue(cell)))
 				})
-			}
 		})
-
-
 		sc.stop()
 	}
 }
-
-
-
